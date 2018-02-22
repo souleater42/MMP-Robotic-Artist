46ac6800cb717a261006e53bb50ad73e1a64f01d
mah60 <mhoward424242@gmail.com>
2018-02-22 16:06:31 +0000

    correcting serial port connection

--- Code/Artists_controller.py
+++ Code/Artists_controller.py
@@ -2,7 +2,7 @@ import serial
 
 if __name__ == "__main__":
 
-    ser = serial.Serial('dev/ttyUSB0', 9600, stopbits=1, parity='E')
+    ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
 
     ser.write(b'PA 500 500;')
     ser.write(b'PA 5000 5000;')

522af6c9a2185d554b4e5fbcb945d49cc93ce9e5
mah60 <mhoward424242@gmail.com>
2018-02-22 16:42:06 +0000

    Created method to read responses, pushing to test

--- Code/Artists_controller.py
+++ Code/Artists_controller.py
@@ -1,14 +1,20 @@
 import serial
 
+def read_all():
+
+    buffer = ""
+    while True:
+        char = ser.read()
+        if char == '\r':
+            break
+        else:
+            buffer += char
+    return buffer
+
+
 if __name__ == "__main__":
 
     ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
 
-    ser.write(b'PA 500 500;')
-    ser.write(b'PA 5000 5000;')
-    ser.write(b'PA 500 500;')
-    ser.write(b'PA 5000 5000;')
-    ser.write(b'PA 500 500;')
-    ser.write(b'PA 5000 5000;')
-    ser.write(b'PA 500 500;')
-    ser.write(b'PA 5000 5000;')
+    ser.write(b'OC;')
+    print(read_all())

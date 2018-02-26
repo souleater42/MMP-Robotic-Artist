e1a787e126a877f8d58027d327194602824c0a88
souleater42 <mah60@aber.ac.uk>
2018-02-26 13:58:41 +0000

    creates two new classes which will handle the GUI and Serial ports ativation. Basic version and untested.

--- Code/Artists_controller.py
+++ Code/Artists_controller.py
@@ -8,40 +8,12 @@ Version => 0.1 - 23/02/2018 - This version is the intial set up for the
 controller. It contains a basic initialization setup and a way to read any
 input.
 """
-import serial
-
-
-def read_all():
-    """
-    Summary => Will be used to read all incoming data from the serial.
-
-    Description => Will be used to read all incoming input from the serail. If
-    there is no input from the serial the session will time out and return
-    to the normal process.
-    """
-    buffer = ""
-    # check if there are any expected input from the serial
-    while ser.in_waiting:
-        # if there is incomming input, throw into loop and
-        # read the loop
-        char = ser.read()
-        # turn byte into ascii
-        char = char.decode("utf-8")
-        # when '\r' appears end reading process
-        if char == '\r':
-            break
-        else:
-            # add to buffer
-            # print(char)
-            buffer += char
-    return buffer
-
+import serial_control
+import GUI
 
 if __name__ == "__main__":
     # setup serial import
-    # TODO :create method to check for other ports
-    ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
-
-    ser.write(b'OC;')
-    print('OC')
-    print(read_all())
+    ser = serial_control('/dev/ttyUSB0')
+    # ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
+    ser.write('OC;')
+    print(ser.read_all())
---
+++ Code/GUI.py
@@ -0,0 +1,27 @@
+"""
+Summary => Will be used to control and generate a GUI.
+
+Description => This class will be used to generate and control the GUI used
+for the system. You will be able to control the camera and see the feed
+comming from it.
+
+Author => Matthew Howard (mah60).
+Version => 0.1 - 24/02/2018 -
+"""
+
+
+class GUI(object):
+        """
+        Summary => maintain the GUI used to control the robotic artist.
+
+        Description =>
+        """
+
+        def __init__(self):
+            """
+            Summary => Will create a GUI object.
+
+            Description =>
+
+            Args =>
+            """

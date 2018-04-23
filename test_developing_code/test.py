155f049e0bb7f96c403d3236d4fdee467c78776e
souleater42 <mah60@aber.ac.uk>
2018-04-23 14:33:29 +0100

    Updated code and templates for documentation

---
+++ test_developing_code/test.py
@@ -0,0 +1,31 @@
+import serial
+'''
+def set_serial():
+    ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
+    if check_serial(ser):
+        return ser
+    else:
+        return serial.Serial('/dev/ttyUSB1', 9600, stopbits=1, parity='E')
+
+def check_serial(ser):
+    ser.write('OC;')
+    fin = 0
+    while(fin == 0):
+        char = ser.read();
+        if char == '\r':
+            fin = 1
+            return true
+        else:
+            print(char)
+'''
+
+
+if __name__ == "__main__":
+    #sets up the serial port, to be able to communicate with the plotter
+    #ser = set_serial()
+    ser = serial.Serial('/dev/ttyUSB0', 9600, stopbits=1, parity='E')
+    ser.write(b"PA 400 400;")
+    ser.write(b"PA 2000 2000;")
+    ser.write(b"PA 5000 500;")
+    ser.write(b"PA 6000 400;")
+    ser.write(b"PA 0 0;")
